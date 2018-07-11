046843e9528677244fd875e7eaa2a74e1f219a24
Adam Quinton <quintonish@gmail.com>
2018-07-10 20:44:39 -0700

    switch to conference quotes

--- app.js
+++ app.js
@@ -38,6 +38,6 @@ function getRandomIndex(arr) {
   return Math.floor(Math.random() * (max + 1));
 }
 
-server.listen(process.env.PORT || 8080, function () {
+server.listen(process.argv[2] || process.env.PORT || 8080, function () {
   console.log('%s listening at %s', server.name, server.url);
 });
